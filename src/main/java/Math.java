7b65d1e4e95818b60dfd57d407901b138dfa5fe9
Enkkurt <enkkurt90@gmail.com>
2021-04-02 08:53:17 -0400

    New class added

---
+++ src/main/java/Math.java
@@ -0,0 +1,14 @@
+public class Math {
+
+    public static void main(String[] args) {
+        System.out.println("Math Class");
+        int num1 = 4;
+        int num2 = 18;
+
+        int numSum = num1+num2 %2;
+
+        boolean check = numSum == 0;
+
+        System.out.println(check);
+    }
+}
